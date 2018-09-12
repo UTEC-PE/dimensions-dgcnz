f72a22318354b6287a8d162a4acc906f1d7bc9da
Diego Canez <canezdiego@gmail.com>
2018-09-11 21:52:25 -0500

    fixed Cesar's main

--- main.cpp
+++ main.cpp
@@ -5,19 +5,51 @@
 
 using namespace std;
 
-template <typename T>
-struct Dimensions {      
-    int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+template <typename T> struct Dimensions {
+  int operator()(int *coordinates, int *sizes, int dimensions) {
+    int term = 1;
+    int final_pos = 0;
+    for (int i = 0; i < dimensions; ++i) {
+      final_pos += coordinates[i] * term;
+      term *= *(sizes + i);
     }
+    return final_pos;
+  }
 };
 
 struct Integer {
-       typedef int T;
-       typedef Dimensions<T> Operation;
+  typedef int T;
+  typedef Dimensions<T> Operation;
 };
 
 int main(int argc, char *argv[]) {
-    system("pause");
-    return EXIT_SUCCESS;
+  // Main, cortesia de Cesar Salcedo
+  int tuple2[] = {2, 3, 5};
+  int tuple[] = {0, 0, 0};
+
+  Vector<Integer> vector(3, tuple2);
+
+  for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 3; j++)
+      for (int k = 0; k < 2; k++) {
+        *tuple = k;
+        *(tuple + 1) = j;
+        *(tuple + 2) = i;
+
+        vector.set(i + j + k, tuple);
+      }
+
+  for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 3; j++)
+      for (int k = 0; k < 2; k++) {
+        *tuple = k;
+        *(tuple + 1) = j;
+        *(tuple + 2) = i;
+        cout << "{" << *tuple << ", " << *(tuple + 1) << ", " << *(tuple + 2)
+             << "} = ";
+
+        cout << vector.get(tuple) << endl;
+      }
+
+  return EXIT_SUCCESS;
 }
--- vector.h
+++ vector.h
@@ -1,29 +1,38 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-template <typename Tr>
-class Vector {
-    public:
-        typedef typename Tr::T T;
-        typedef typename Tr::Operation Operation;
-             
-    private:
-        T* data;
-        int dataSize;
+template <typename Tr> class Vector {
+public:
+  typedef typename Tr::T T;
+  typedef typename Tr::Operation Operation;
 
-        int dimensions;
-        int* dimensionSizes;
+private:
+  T *data;
+  int dataSize;
+  Operation op;
+  int dimensions;
+  int *dimensionSizes;
 
-    public:
-        Vector() : data(nullptr) {};
-             
-        Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
-            // TODO
-        }
-             
-        void set(T datum, int* coordinates); // TODO
-             
-        T get(int* coordinates); // TODO
+public:
+  Vector() : data(nullptr){};
+
+  Vector(int dimensions, int *dimensionSizes)
+      : dimensions(dimensions), dimensionSizes(dimensionSizes) {
+    for (int i = 0; i < dimensions; ++i) {
+      this->dataSize *= *(dimensionSizes + i);
+    }
+    this->data = new int[this->dataSize];
+  }
+
+  void set(T datum, int *coordinates) {
+
+    this->data[op(coordinates, this->dimensionSizes, this->dimensions)] = datum;
+  }
+
+  T get(int *coordinates) {
+    return data[op(coordinates, this->dimensionSizes, this->dimensions)];
+  }
+  ~Vector() { delete[] this->data; }
 };
 
 #endif
